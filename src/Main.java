7c7edefa6f3214dcb1ecaaa885a72cb06b857356
ENoT51 <ambarov515@gmail.com>
2024-03-24 16:13:31 +0300

    homework5

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,43 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+        for (int i = 0; i < 17; i = i + 2) {
+            System.out.println(i);
+        }
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + " год является високосным");
+        }
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
+        }
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println(i);
+        }
+        int value = 29000;
+        int total = 0;
+        for (int i = 1; i < 13; i++) {
+            total = total + value;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей");
+        }
+        int value2 = 29000;
+        int total2 = 0;
+        for (int i = 1; i < 13; i++) {
+            total2 = total2 + total2 / 100;
+            total2 = total2 + value2;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + total2 + " рублей");
+        }
+        for (int i =1; i <= 10; i++){
+            int y = i * 2;
+            System.out.println("2*" + i + " = " + y);
         }
     }
 }
